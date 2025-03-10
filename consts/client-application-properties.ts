41be612bc3aa7fc96062fa54b741e4b9e4141d4d
hanzochang <hanzo.chan@gmail.com>
2025-03-10 12:28:08 +0900

    feat: update api url

--- consts/client-application-properties.ts
+++ consts/client-application-properties.ts
@@ -1,5 +1,5 @@
 export const clientApplicationProperties = {
-  appUrl: process.env.NEXT_PUBLIC_APP_URL!,
+  appUrl: process.env.NEXT_PUBLIC_API_URL!,
   privyAppId: process.env.NEXT_PUBLIC_PRIVY_APP_ID!,
   dailyPromptUsageLimit: {
     perUser: Number(process.env.NEXT_PUBLIC_DAILY_PROMPT_USAGE_LIMIT_PER_USER) || 50,
