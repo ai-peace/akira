c119e305fd15de9f91edcf12d970c3225d4501bd
hanzochang <hanzo.chan@gmail.com>
2025-03-15 23:08:21 +0900

    feat: ログインできない時に赤色にした

--- utils/error-handler.helper.ts
+++ utils/error-handler.helper.ts
@@ -5,9 +5,9 @@ const handleError = (error: unknown, options?: { description?: string }) => {
   const errorOptions = {
     ...options,
     style: {
-      backgroundColor: '#FEE2E2', // 薄い赤色の背景
-      border: '1px solid #FCA5A5', // 赤色のボーダー
-      color: '#B91C1C', // 濃い赤色のテキスト
+      backgroundColor: 'hsl(346.8 77.2% 49.8%)', // 薄い赤色の背景
+      border: '1px solid hsl(346.8 77.2% 49.8%)', // 赤色のボーダー
+      color: 'var(--foreground)', // 濃い赤色のテキスト
     },
   }
 
