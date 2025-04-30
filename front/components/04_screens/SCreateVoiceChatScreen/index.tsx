7e9ed2a4951b35fa76b5229c7a28acab7497251a
hanzochang <hanzo.chan@gmail.com>
2025-05-01 03:23:32 +0900

    commit for deploying

--- front/components/04_screens/SCreateVoiceChatScreen/index.tsx
+++ front/components/04_screens/SCreateVoiceChatScreen/index.tsx
@@ -6,6 +6,7 @@ import { voiceChatUrl } from '@/front/util/url.helper'
 import { useRouter } from 'next/navigation'
 import { FC, useEffect } from 'react'
 
+// NOTE この画面は、VoiceChatを作成するための画面です。
 const Component: FC = () => {
   const router = useRouter()
   const { createVoiceChat } = useCreateVoiceChat()
